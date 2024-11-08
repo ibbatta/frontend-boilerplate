a7ea46d7bba6322908a9fd812b6ebf6ee69d23ca
Maurizio Battaghini <maurizio.battaghini+tech@gmail.com>
2024-11-08 14:39:25 +0100

    routing and folder three settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+import Core from "./modules/Core";
+
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen bg-slate-200 gap-3">
-      <h1 className="text-5xl  text-center text-gray-800">Hello world!</h1>
-      <h2 className="uppercase text-2xl text-gray-400">SPA-BOILERPLATE</h2>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Core />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/modules/Core/index.tsx
@@ -0,0 +1,9 @@
+function Core() {
+  return (
+    <div>
+      <h1>Core</h1>
+    </div>
+  );
+}
+
+export default Core;
