86444bbe32b30b50de667c40f1ca890d9d58f1d6
Maurizio Battaghini <maurizio.battaghini+tech@gmail.com>
2024-11-14 22:49:38 +0100

    prepare core template

--- src/modules/Core/index.tsx
+++ src/modules/Core/index.tsx
@@ -1,7 +1,8 @@
 function Core() {
   return (
-    <div>
-      <h1>Core</h1>
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-2 bg-slate-300">
+      <h1 className="font-thin text-4xl text-slate-900">Hello World!</h1>
+      <h2 className="font-bold uppercase text-slate-600">SPA Boilerplate</h2>
     </div>
   );
 }
